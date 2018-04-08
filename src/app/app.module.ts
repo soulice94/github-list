145da596c8ac349328c87c0b125affc6a936c190
Pedro Arturo Flores Ramirez <pedro@MacBook-Pro.local>
2018-04-08 15:51:33 -0500

    NgBootstrap, Bootstrap y Navbar
    - Se añadió el paquete de NgBootstrap para Angular 5
    - Se agregó el CSS de Bootstrap 4
    - Se añadió una navbar para el sitio en el componente principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
 
 import { AppComponent } from './app.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusquedaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
